15994cedf829849d2a632e106c158b568c541f8e
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-07-21 16:48:20 -0300

    ci: tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#E8AA42',
+        secondary: '#025464',
+        accent: '#E57C23',
+        text: '#0F1B1F',
+        background: '#F7F7F7',
+      },
+      screens: {
+        xs: '370px',
+        sm: '540px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        xxl: '1440px',
+      },
+      fontFamily: {
+        text: ['Poppins', 'sans-serif'],
+        title: ['Rubik', 'sans-serif'],
+        logo: ['Lumanosimo', 'cursive'],
+      },
+    },
   },
   plugins: [],
 };
