0b310f3a6271d2880877c6187052bf9e589e330a
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-07-21 19:07:32 -0300

    created blog app

---
+++ src/blog/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from landing.models import CustomUser
+
+# Create your models here.
+
+
+class Recipe():
+    title = models.CharField(max_length=200)
+    description = models.TextField(max_length=200)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    likes_count = models.IntegerField(default=0)
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    # banner = models.ImageField(upload_to='banners/', null=True, blank=True)
+    # pictures = models.ImageField(upload_to='pictures/', null=True, blank=True)
+
+
+class Conversation():
+    participant1 = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name='conversations_as_participant1')
+    participant2 = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name='conversations_as_participant2')
+
+
+class Message():
+    conversation = models.ForeignKey(
+        Conversation, on_delete=models.CASCADE, related_name='messages')
+    sender = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
