1dd249e1bae7aba737e3623d6a1155ffec92302a
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-07-21 19:07:47 -0300

    custom user model

--- src/landing/models.py
+++ src/landing/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from blog.models import Recipe
 
 # Create your models here.
+
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(max_length=254, unique=True)
+    username = models.CharField(max_length=30, unique=True)
+    name = models.CharField(max_length=100, null=True, blank=True)
+    # avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+    recipes = models.ManyToManyField(Recipe, related_name='recipes')
+    favorites = models.ManyToManyField(Recipe, related_name='favorites')
