a068a76424ba7005eec1affd2b66c1789109afca
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-07-21 22:07:08 -0300

    fixed models and migrations

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,17 +1,16 @@
 from django.db import models
-from landing.models import CustomUser
 
 # Create your models here.
 
 
-class Recipe():
+class Recipe(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(max_length=200)
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     likes_count = models.IntegerField(default=0)
-    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    author = models.ForeignKey('landing.CustomUser', on_delete=models.CASCADE)
     # banner = models.ImageField(upload_to='banners/', null=True, blank=True)
     # pictures = models.ImageField(upload_to='pictures/', null=True, blank=True)
 
@@ -22,17 +21,17 @@ class Recipe():
         return f"{self.title}"
 
 
-class Conversation():
+class Conversation(models.Model):
     participant1 = models.ForeignKey(
-        CustomUser, on_delete=models.CASCADE, related_name='conversations_as_participant1')
+        'landing.CustomUser', on_delete=models.CASCADE, related_name='conversations_as_participant1')
     participant2 = models.ForeignKey(
-        CustomUser, on_delete=models.CASCADE, related_name='conversations_as_participant2')
+        'landing.CustomUser', on_delete=models.CASCADE, related_name='conversations_as_participant2')
 
 
-class Message():
+class Message(models.Model):
     conversation = models.ForeignKey(
         Conversation, on_delete=models.CASCADE, related_name='messages')
-    sender = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    sender = models.ForeignKey('landing.CustomUser', on_delete=models.CASCADE)
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
--- src/landing/models.py
+++ src/landing/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from blog.models import Recipe
 
 # Create your models here.
 
@@ -10,12 +9,13 @@ class CustomUser(AbstractUser):
     name = models.CharField(max_length=100, null=True, blank=True)
     # avatar = models.ImageField(upload_to='avatars/', null=True, blank=True, default='avatars/default.png')
     bio = models.TextField(null=True, blank=True)
-    recipes = models.ManyToManyField(Recipe, related_name='recipes')
-    favorites = models.ManyToManyField(Recipe, related_name='favorites')
+    recipes = models.ManyToManyField('blog.Recipe', related_name='recipes')
+    favorites = models.ManyToManyField('blog.Recipe', related_name='favorites')
     liked_recipes = models.ManyToManyField(
-        Recipe, related_name='liked_recipes')
+        'blog.Recipe', related_name='liked_recipes')
 
     USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
     def __str__(self):
         return f"{self.username}"
