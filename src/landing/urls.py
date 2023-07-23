d814e68f9d4271d16b738285598efe65b79e3e47
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-07-22 23:01:40 -0300

    register url and view

--- src/landing/urls.py
+++ src/landing/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.landing, name="landing"),
     path("login/", views.login, name="login"),
+    path("register/", views.register, name="register"),
 ]
--- src/landing/views.py
+++ src/landing/views.py
@@ -4,8 +4,12 @@ from django.shortcuts import render
 
 
 def landing(request):
-    return render(request, "landing/landing_page.html")
+    return render(request, "landing/landing.html")
 
 
 def login(request):
     return render(request, "landing/login.html")
+
+
+def register(request):
+    return render(request, "landing/register.html")
