16c00c8899d1f175fba8671e03aa1ff122fb4a4c
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-07-27 18:21:40 -0300

    upload image and show it

--- src/static/scripts/landing/profile-picture.js
+++ src/static/scripts/landing/profile-picture.js
@@ -5,4 +5,16 @@ const fileInput = document.getElementById('file-input');
 
 profilePicContainer.addEventListener('click', () => {
   fileInput.click();
+
+  fileInput.addEventListener('change', () => {
+    const file = fileInput.files[0];
+    const reader = new FileReader();
+
+    reader.addEventListener('load', () => {
+      const profileImage = document.getElementById('profile-image');
+      profileImage.src = reader.result;
+    });
+
+    reader.readAsDataURL(file);
+  });
 });
