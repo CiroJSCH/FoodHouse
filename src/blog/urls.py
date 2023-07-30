6a821ba87de4b404efb2f16a339fb7b2d358c6c1
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-07-30 19:01:38 -0300

    favorites url

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path('', views.home, name='home'),
+    path('/favorites', views.home, name='favorites'),
     path('chat/', views.chat, name='chat'),
 ]
